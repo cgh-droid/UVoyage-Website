1262f9d198e8affe5c67c6e55a3598ec4d2accf1
cgh-droid <174542363+cgh-droid@users.noreply.github.com>
2025-03-26 22:16:29 -0400

    our team and story changes

--- UVoyageWebsiteMVC/UVoyageWebsiteMVC/obj/Debug/net8.0/UVoyageWebsiteMVC.AssemblyInfo.cs
+++ UVoyageWebsiteMVC/UVoyageWebsiteMVC/obj/Debug/net8.0/UVoyageWebsiteMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UVoyageWebsiteMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb425dd93552aede71a57b48f8aa4e0b1c55eba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43626f3e7b396ccce21aa1d9759a8cf817b38ae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UVoyageWebsiteMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UVoyageWebsiteMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
